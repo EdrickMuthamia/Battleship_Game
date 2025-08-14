f411a98683c4437e2cda382aa36285e02f4a41be
Jane Nduku Kimei <Janetnduku519@gmail.com>
2025-08-14 16:16:05 +0300

    Add menu system for character selection

---
+++ jane.py
@@ -0,0 +1,11 @@
+# -----------------------------------
+# Task 2 - Menu System (Jane)
+# -----------------------------------
+
+while True:
+    print("Choose your character:")
+    print("1) Wizard")
+    print("2) Elf")
+    print("3) Human")
+
+    character_choice = input("Select your character: ")
