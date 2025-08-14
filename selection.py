b4c7906271b4ada29326651e26752a9eaff4fb96
feysalabdi51 <pheysalupdyy@gmail.com>
2025-08-14 16:19:11 +0300

    update

--- selection.py
+++ selection.py
@@ -1,19 +1,19 @@
-    if character_choice == "1":
+if character_choice == "1":
         char = wizard
         player_hp = wizard_hp
         my_damage = wizard_damage
         break
-    elif character_choice == "2":
+     elif character_choice == "2":
         char = elf
         player_hp = elf_hp
         my_damage = elf_damage
         break
-    elif character_choice == "3":
+     elif character_choice == "3":
         char = human
         player_hp = human_hp
         my_damage = human_damage
         break
-    else:
+else:
         print("Invalid choice. Please try again.\n")
 
 print(f"\nYou have chosen the {char}!\n")
